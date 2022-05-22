6670dff47e6092dc4c330ac4617ef2f8178185b6
Agraphie <agraphie@mailbox.org>
2022-05-22 09:45:39 +0200

    Add point in time capability

--- src/main/java/com/github/agraphie/elasticsearch/SearchAfterSpliterator.java
+++ src/main/java/com/github/agraphie/elasticsearch/SearchAfterSpliterator.java
@@ -1,26 +1,28 @@
 package com.github.agraphie.elasticsearch;
 
+import java.util.Arrays;
+import java.util.Spliterator;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
 import org.apache.lucene.search.TotalHits;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.PointInTimeBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-
-import java.util.Arrays;
-import java.util.Spliterator;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Consumer;
+import org.elasticsearch.search.slice.SliceBuilder;
 
 class SearchAfterSpliterator implements Spliterator<SearchHit> {
+
   private final String[] indices;
   private final ESClient esClient;
   private final SearchSourceBuilder searchSourceBuilder;
-  private final AtomicInteger currentIndex = new AtomicInteger(0);
+  private int currentIndex;
   private final ReentrantLock reentrantLock;
   private volatile Object[] lastSortValues;
   private SearchHits currentHits;
+  private final PointInTimeBuilder pointInTimeBuilder;
 
   SearchAfterSpliterator(
       SearchSourceBuilder searchRequestBuilder, ESClient esClient, String... indices) {
@@ -32,6 +34,19 @@ class SearchAfterSpliterator implements Spliterator<SearchHit> {
     this.searchSourceBuilder = searchRequestBuilder;
     this.esClient = esClient;
     this.reentrantLock = new ReentrantLock();
+    this.pointInTimeBuilder = null;
+  }
+
+  SearchAfterSpliterator(
+      SearchSourceBuilder searchRequestBuilder,
+      PointInTimeBuilder pointInTimeBuilder,
+      ESClient esClient) {
+    this.indices = null;
+    this.searchSourceBuilder =
+        searchRequestBuilder.shallowCopy().pointInTimeBuilder(pointInTimeBuilder);
+    this.esClient = esClient;
+    this.reentrantLock = new ReentrantLock();
+    this.pointInTimeBuilder = pointInTimeBuilder;
   }
 
   private SearchAfterSpliterator(
@@ -40,17 +55,23 @@ class SearchAfterSpliterator implements Spliterator<SearchHit> {
       SearchHits currentHits,
       Object[] lastSortValues,
       ReentrantLock reentrantLock,
+      PointInTimeBuilder pointInTimeBuilder,
       String... indices) {
     if (searchRequestBuilder.sorts() == null || searchRequestBuilder.sorts().isEmpty()) {
       throw new IllegalArgumentException(
           "Need to set at least one sort field for search after searches!");
     }
-    this.indices = indices.clone();
+    if (indices != null) {
+      this.indices = indices.clone();
+    } else {
+      this.indices = null;
+    }
     this.searchSourceBuilder = searchRequestBuilder;
     this.esClient = esClient;
     this.currentHits = currentHits;
     this.lastSortValues = lastSortValues;
     this.reentrantLock = reentrantLock;
+    this.pointInTimeBuilder = pointInTimeBuilder;
   }
 
   @Override
@@ -59,7 +80,7 @@ class SearchAfterSpliterator implements Spliterator<SearchHit> {
       getNextPage();
     }
 
-    action.accept(currentHits.getAt(currentIndex.getAndIncrement()));
+    action.accept(currentHits.getAt(currentIndex++));
     getNextPageIfAtEndOfCurrent();
 
     return elementsLeft();
@@ -73,18 +94,24 @@ class SearchAfterSpliterator implements Spliterator<SearchHit> {
     }
     SearchHit[] hits = currentHits.getHits();
     int size = hits.length;
-    int splitPos = size / 2 + currentIndex.get();
-    SearchHit[] splitHits = Arrays.copyOfRange(hits, currentIndex.get(), splitPos);
+    int splitPos = size / 2 + currentIndex;
+    SearchHit[] splitHits = Arrays.copyOfRange(hits, currentIndex, splitPos);
     if (splitHits.length == 0) {
       return null;
     }
 
-    currentIndex.set(splitPos);
+    currentIndex = splitPos;
     SearchHits newSpliterator =
         new SearchHits(splitHits, currentHits.getTotalHits(), currentHits.getMaxScore());
 
     return new SearchAfterSpliterator(
-        searchSourceBuilder, esClient, newSpliterator, lastSortValues, reentrantLock, indices);
+        searchSourceBuilder,
+        esClient,
+        newSpliterator,
+        lastSortValues,
+        reentrantLock,
+        pointInTimeBuilder,
+        indices);
   }
 
   @Override
@@ -106,11 +133,11 @@ class SearchAfterSpliterator implements Spliterator<SearchHit> {
 
   private boolean elementsLeft() {
     return currentHits != null
-        && (currentHits.getHits().length > 0 && currentIndex.get() < currentHits.getHits().length);
+        && (currentHits.getHits().length > 0 && currentIndex < currentHits.getHits().length);
   }
 
   private void getNextPageIfAtEndOfCurrent() {
-    if (currentHits != null && currentIndex.get() < currentHits.getHits().length) {
+    if (currentHits != null && currentIndex < currentHits.getHits().length) {
       return;
     }
 
@@ -118,23 +145,28 @@ class SearchAfterSpliterator implements Spliterator<SearchHit> {
   }
 
   private void getNextPage() {
-    if (!reentrantLock.tryLock()) {
-      return;
-    }
+    try {
+      reentrantLock.lock();
+      final SearchRequest searchRequest;
+      if (indices == null || indices.length == 0) {
+        searchRequest = new SearchRequest();
+      } else {
+        searchRequest = new SearchRequest(indices);
+      }
+      if (lastSortValues != null) {
+        searchSourceBuilder.searchAfter(lastSortValues);
+      }
 
-    if (lastSortValues != null) {
-      searchSourceBuilder.searchAfter(lastSortValues);
+      searchRequest.source(searchSourceBuilder);
+      SearchResponse currentResponse = esClient.search(searchRequest);
+      currentHits = currentResponse.getHits();
+      if (currentHits != null && currentHits.getHits().length > 0) {
+        SearchHit lastHit = currentHits.getAt(currentHits.getHits().length - 1);
+        lastSortValues = lastHit.getSortValues();
+      }
+    } finally {
+      reentrantLock.unlock();
     }
-
-    SearchRequest searchRequest = new SearchRequest(indices);
-    searchRequest.source(searchSourceBuilder);
-    SearchResponse currentResponse = esClient.search(searchRequest);
-    currentHits = currentResponse.getHits();
-    if (currentHits != null && currentHits.getHits().length > 0) {
-      SearchHit lastHit = currentHits.getAt(currentHits.getHits().length - 1);
-      lastSortValues = lastHit.getSortValues();
-    }
-    reentrantLock.unlock();
-    currentIndex.set(0);
+    currentIndex = 0;
   }
 }
